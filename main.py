656be57ea675d5a0d562a43f3e1fcb1c45d31c30
nitzan-elad <nitzan.elad@gmail.com>
2025-05-29 08:23:45 +0300

    +Fixed Parsing bug +reorganized project files
    -removed subject.py

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from parsing import ChatParser
 from filtering import ChatFormatter
 import re
 from datetime import datetime
+import pandas as pd
 
 if __name__ == "__main__":
     algebra = ChatParser(r"Whatsapp_Groups\algebra.txt")
 
     algebra = algebra.set_df()
 
+    algebra.to_csv("test3.csv", index=False, encoding='utf-8-sig')
     start = "2022-11-11"
     end = "2023-03-10"
+    #
+    # algebra = ChatFormatter(algebra)
+    # algebra.set_start_date(start)
+    # algebra.set_end_date(end)
+    #
 
-    algebra = ChatFormatter(algebra)
-    algebra.set_start_date(start)
-    algebra.set_end_date(end)
-
-    print(algebra.df.head(100))
 
 
